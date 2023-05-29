25782a357f6a6604571d3a5b5f40543d2c24474e
Mohamed Sayed <61032197+MohamedSayedK@users.noreply.github.com>
2023-05-29 22:07:29 +0200

    Completed desktop design for the portfolio

--- app.js
+++ app.js
@@ -32,3 +32,40 @@ screen.onmouseenter = event => {
   }, 30);
 }
 
+const elements = document.querySelectorAll("#typing-effect");
+const speed = 50;
+
+function type(element, index) {
+  const text = element.getAttribute("data-text");
+  let charIndex = 0;
+  
+  function typeChar() {
+    if (charIndex < text.length) {
+      element.innerHTML += text.charAt(charIndex);
+      charIndex++;
+      setTimeout(typeChar, speed);
+    } else {
+      index++;
+      if (index < elements.length) {
+        setTimeout(() => type(elements[index], index), 300);
+      }
+    }
+  }
+  
+  setTimeout(typeChar, 300);
+}
+
+type(elements[0], 0);
+
+
+
+document.querySelector(".projects").onmousemove = e => {
+  for(const project of document.getElementsByClassName("project")) {
+    const rect = project.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    project.style.setProperty("--mouse-x", `${x}px`);
+    project.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
